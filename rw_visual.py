d1dc052b5faccfea2d10475e89532c678c94d7e0
ivane98 <i.vardoshvili22@gmail.com>
2024-02-22 11:01:11 +0400

    2202

---
+++ rw_visual.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+while True:
+
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    plt.style.use('classic')
+
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=2)
+    ax.set_aspect('equal')
+
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input('Make Another Plot? (y/n)')
+
+    if keep_running == 'n':
+        break
